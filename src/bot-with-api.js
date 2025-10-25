64973c15ab1c4cd6cea1583a67beb1f8d7fb6a6e
Mikhail <mikhailali2010@gmail.com>
2025-10-25 12:37:07 -0700

    Added command registration script and all updates

--- src/bot-with-api.js
+++ src/bot-with-api.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, EmbedBuilder, ChannelType, PermissionFlagsBits, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
